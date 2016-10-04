b57b54a0e9244423336d0e048a3790b13cc21f21
lundgren84 <lundgren84@hotmail.se>
2016-10-04 18:19:41 +0200

    J

---
+++ Utopish_Space/Utopish_Space/Models/PlayerObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Utopish_Space.Models
+{
+    public class PlayerObject
+    {
+        public string _email { get; set; }
+    }
+}
