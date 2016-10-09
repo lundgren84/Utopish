18eb3a32ea7190d9563fce17e880fbf236f50990
KevinKarlberg <Kevinkarlberg@hotmail.se>
2016-10-09 06:44:23 +0200

    Troops/Ships/Resouces/Research done in codefirst

--- UtopishDataBase/ProjectSpaceDatabase/Tables/Ships/PlayerShips.cs
+++ UtopishDataBase/ProjectSpaceDatabase/Tables/Ships/PlayerShips.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjectSpaceDatabase
 {
     public class PlayerShips
     {
+        [Key, Column(Order = 0)]
+        public int PlayerID { get; set; }
+
+
+        [Key, Column(Order = 1)]
+        public int ShipsID { get; set; }
+
+
+        public virtual Player Player { get; set; }
+        public virtual Ships Ships { get; set; }
+        [Required]
+        public int Quantity { get; set; }
     }
 }
--- UtopishDataBase/ProjectSpaceDatabase/Tables/Ships/Ships.cs
+++ UtopishDataBase/ProjectSpaceDatabase/Tables/Ships/Ships.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjectSpaceDatabase
 {
     public class Ships
     {
+        [Required]
+        public int ShipsID { get; set; }
+        [Required]
+        public string ShipName { get; set; }
+
     }
 }
