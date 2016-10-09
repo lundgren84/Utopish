b87a3c29e37085973c778b164dba0567ffede86c
lundgren84 <lundgren84@hotmail.se>
2016-10-09 07:45:59 +0200

    asd

---
+++ UtopishDataBase/ProjectSpaceDatabase/ProjectSpaceDataBaseDBContect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ProjectSpaceDatabase
+{
+    public class ProjectSpaceDataBaseDBContect: DbContext
+    {
+
+            public ProjectSpaceDataBaseDBContect() : base("name =ProjectSpaceDataBaseDBContectConectionString")
+            {
+
+
+            }
+           // public DbSet<KlassNamn> KlassNamn { get; set; }
+       
+        }
+
+    
+}
