b57e0dd9938776a854c05983f077b5c140ba5edf
lundgren84 <lundgren84@hotmail.se>
2016-10-09 09:57:27 +0200

    asd

--- UtopishDataBase/UtopishDataBase/Tables/Planets/Terrain.cs
+++ UtopishDataBase/UtopishDataBase/Tables/Planets/Terrain.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Web;
 
-namespace ProjectSpaceDatabase
+namespace UtopishDataBase
 {
     public class Terrain
     {
