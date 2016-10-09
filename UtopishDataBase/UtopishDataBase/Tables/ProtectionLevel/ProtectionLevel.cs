b8727f6e6800350e408aa32b3180635bd928fe18
KevinKarlberg <Kevinkarlberg@hotmail.se>
2016-10-09 11:19:09 +0200

    Added ProtectionLevel

---
+++ UtopishDataBase/UtopishDataBase/Tables/ProtectionLevel/ProtectionLevel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using UtopishDatabase;
+
+
+namespace UtopishDataBase
+{
+    public class ProtectionLevel
+    {
+        [Required]
+        public int ProtectionLevelID { get; set; }
+        [Required]
+        public int ProtectionLevelName { get; set; }
+        public DateTime Expires { get; set; }
+    }
+}
