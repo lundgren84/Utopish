37cd6e8499f337e4bf42750cbbd319a79f157c75
KevinKarlberg <Kevinkarlberg@hotmail.se>
2016-10-09 06:29:05 +0200

    Troops done

--- UtopishDataBase/ProjectSpaceDatabase/Tables/Troops/PlayerTroops.cs
+++ UtopishDataBase/ProjectSpaceDatabase/Tables/Troops/PlayerTroops.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjectSpaceDatabase
 {
     public class PlayerTroops
     {
+        [Required]
+        public int Quantity { get; set; }
+        //-----------ForenKeys------------
+        //Location
+        public int PlayerRefID { get; set; }
+        [ForeignKey(name: "PlayerID")]
+
+        [Required]
+        public int TroopsRefID { get; set; }
+        [ForeignKey(name: "TroopsID")]
+
     }
 }
