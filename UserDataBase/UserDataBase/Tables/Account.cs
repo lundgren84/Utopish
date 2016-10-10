696fdf9132ca6a389da8c407a4a3bfc52dbe2917
lundgren84 <lundgren84@hotmail.se>
2016-10-10 05:39:33 +0200

    asd

---
+++ UserDataBase/UserDataBase/Tables/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserDataBase.Tables
+{
+    public class Account
+    {
+        [Key]
+        public int AccountID { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        public string Salt { get; set; }
+        public string Hash { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        //FK
+        public int? StatusRefID { get; set; }
+        [ForeignKey(name: "StatusRefID")]
+        public virtual Status Status { get; set; }
+
+
+    }
+}
