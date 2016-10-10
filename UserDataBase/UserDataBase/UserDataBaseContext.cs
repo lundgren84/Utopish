3c28d1462068f69c35547d637d665e3e860676ff
lundgren84 <lundgren84@hotmail.se>
2016-10-10 05:31:01 +0200

    UserDB Basics

---
+++ UserDataBase/UserDataBase/UserDataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UserDataBase
+{
+    public class UserDataBaseContext : DbContext
+    {
+            public UserDataBaseContext() : base("name =UserDataBaseContextConectionString")
+            {
+
+            }      
+    }
+}
