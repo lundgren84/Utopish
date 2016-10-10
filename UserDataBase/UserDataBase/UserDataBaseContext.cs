824d76ce9d4030a19597ee89f3215d8d14dbd933
lundgren84 <lundgren84@hotmail.se>
2016-10-10 05:47:28 +0200

    asd

--- UserDataBase/UserDataBase/UserDataBaseContext.cs
+++ UserDataBase/UserDataBase/UserDataBaseContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using UserDataBase.Tables;
 
 namespace UserDataBase
 {
@@ -11,6 +12,8 @@ namespace UserDataBase
             public UserDataBaseContext() : base("name =UserDataBaseContextConectionString")
             {
 
-            }      
+            }
+        public DbSet<Status> Status { get; set; }
+        public DbSet<Account> Account { get; set; }
     }
 }
