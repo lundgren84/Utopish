5c7737a695329e9baf9d0ce08d24ff642ace1f0d
KevinKarlberg <Kevinkarlberg@hotmail.se>
2016-10-11 09:36:25 +0200

    Solved connectionstring error

--- Utopish_Space/Utopish_Space/DAL/DBConnection.cs
+++ Utopish_Space/Utopish_Space/DAL/DBConnection.cs
@@ -9,8 +9,8 @@ namespace Utopish_Space.DAL
     public class DBConnection
     {
         public SqlConnection connection = new SqlConnection();
-        private string connectionString = "@Data source = 217.210.151.153,1433; Network Library = DBMSSOCN; Initial Catalog = ProjectSpace; User ID = guest; Password=temppass22";
-        private string connectionStringLogin = "@Data source = 217.210.151.153,1433; Network Library = DBMSSOCN; Initial Catalog = ProjectSpaceUserDataBase; User ID = guest; Password=temppass22";
+        private string connectionString = @"Data source=217.210.151.153,1433; Network Library=DBMSSOCN; Initial Catalog=ProjectSpace; User ID=guest; Password=temppass22";
+        private string connectionStringLogin = @"Data source=217.210.151.153,1433; Network Library=DBMSSOCN; Initial Catalog=ProjectSpaceUserDataBase; User ID=guest; Password=temppass22";
 
         public void Open()
         {
