f8a43a4a7aed78d518e4cd54107e9b7058f5d99c
lundgren84 <lundgren84@hotmail.se>
2016-10-11 09:36:47 +0200

    asd

---
+++ Utopish_Space/Utopish_Space/Models/TheGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Utopish_Space.Models
+{
+    public class TheGame
+    {
+        internal PlayerObject CreateNewPlayerObject(AccountObject acc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Utopish_Space/Utopish_Space/UserPages/VerifyMail.aspx.cs
+++ Utopish_Space/Utopish_Space/UserPages/VerifyMail.aspx.cs
@@ -14,15 +14,21 @@ namespace Utopish_Space.UserPages
         Models.Login login = new Models.Login();
         protected void Page_Load(object sender, EventArgs e)
         {
-            acc = (AccountObject)Session["Player"] ;
+            acc = (AccountObject)Session["Player"];
         }
 
         protected void Button_Unlock_Click(object sender, EventArgs e)
         {
             if (TextBox_ActivationCode.Text == acc.Status.ActivationCode)
             {
-                login.ChangeAccountStatus("Open", acc._statusRefID);
-                Response.Redirect("~/UserPages/Overview.aspx");
+                if (acc.Status.AccountStatus == "VerifyEmail")
+                {
+                    login.ChangeAccountStatus("Open", acc._statusRefID);
+                    Response.Redirect("~/UserPages/Overview.aspx");
+                    TheGame theGame = new TheGame();
+                    PlayerObject playerObject = theGame.CreateNewPlayerObject(acc);
+
+                }
             }
             else
             {
