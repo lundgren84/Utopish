4eb865bff057337e78d255c9f12ab2d6e8608e5d
lundgren84 <lundgren84@hotmail.se>
2016-10-11 10:50:56 +0200

    asd

---
+++ Utopish_Space/Utopish_Space/Models/RaceObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Utopish_Space.Models
+{
+    public class RaceObject
+    {
+        public string RaceName { get; set; }
+        public string History { get; set; }
+        public List<string> BonusList { get; set; }
+    }
+}
